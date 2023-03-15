0aad14f9b5911cf615ff44b8ed8a94c17a56c202
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-03-15 23:45:14 +0200

    First commit

---
+++ src/learning/vladdubceac/memento/Caretaker.java
@@ -0,0 +1,16 @@
+package learning.vladdubceac.memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Caretaker {
+    private List<Memento> mementoList = new ArrayList<Memento>();
+
+    public void add(Memento memento){
+        mementoList.add(memento);
+    }
+
+    public Memento get(int index){
+        return mementoList.get(index);
+    }
+}
---
+++ src/learning/vladdubceac/memento/Memento.java
@@ -0,0 +1,13 @@
+package learning.vladdubceac.memento;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
